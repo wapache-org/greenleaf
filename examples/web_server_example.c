2a979c49df0705f65f00ce03a5ed5387d9bea477
ykuang <ykuang@wapache.org>
2020-01-28 15:49:26 +0800

    add cli opts to the web server example

--- examples/web_server_example.c
+++ examples/web_server_example.c
@@ -1,7 +1,16 @@
 // Copyright (c) 2015 Cesanta Software Limited
 // All rights reserved
 
+//header for getopt
+#include <unistd.h>
+//header for getopt_long
+#include <getopt.h>
+
 #include "mongoose.h"
+
+static void usage(int argc, char* argv[]);
+void parse_options(struct mg_serve_http_opts* opts, int argc, char* argv[]);
+
 
 static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
@@ -12,7 +21,10 @@ static void ev_handler(struct mg_connection *nc, int ev, void *p) {
   }
 }
 
-int main(void) {
+int main(int argc, char* argv[]) {
+
+  parse_options(&s_http_server_opts, argc,argv);
+
   struct mg_mgr mgr;
   struct mg_connection *nc;
 
@@ -26,8 +38,6 @@ int main(void) {
 
   // Set up HTTP server parameters
   mg_set_protocol_http_websocket(nc);
-  s_http_server_opts.document_root = ".";  // Serve current directory
-  s_http_server_opts.enable_directory_listing = "yes";
 
   for (;;) {
     mg_mgr_poll(&mgr, 1000);
@@ -35,4 +45,64 @@ int main(void) {
   mg_mgr_free(&mgr);
 
   return 0;
+}
+
+// 短选项字符串, 一个字母表示一个短参数, 如果字母后带有冒号, 表示这个参数必须带有参数
+// 建议按字母顺序编写
+static char* short_opts = "hlp:r:";
+// 长选项字符串, {长选项名字, 0:没有参数|1:有参数|2:参数可选, flags, 短选项名字}
+// 建议按长选项字母顺序编写
+static const struct option long_options[] = {
+		{"enable-directory-listing",0,NULL,'l'},
+		{"help",0,NULL,'h'},
+		{"port",1,NULL,'p'},
+		{"root",1,NULL,'r'}
+};
+// 打印选项说明
+static void usage(int argc, char* argv[])
+{
+  printf("Usages: \n");
+  printf("    %s -p 8080 -r html\n", argv[0]);
+  printf("Options:\n");
+  printf("    [-%s, --%s]     %s\n", "h","help","print this message");
+  printf("    [-%s, --%s]     %s\n", "p","poot","web server bingding port, default is 8000.");
+  printf("    [-%s, --%s]     %s\n", "r","root","web server root directory, default is current work directory.");
+  printf("    [-%s, --%s]     %s\n", "l","enable-directory-listing","if cannot find index file, list directory files, default is no.");
+}
+// 解析选项
+void parse_options(struct mg_serve_http_opts* opts, int argc, char* argv[])
+{
+
+  // 先设置默认值
+  opts->enable_directory_listing = "no";
+
+  // 如果只有短选项, 用getopt就够了
+  // int code = getopt(argc, argv, short_opts);
+
+  // 解析长选项和短选项
+  int opt = 0;
+  while((opt=getopt_long(argc,argv,short_opts,long_options,NULL))!=-1){
+    switch (opt)
+    {
+    case 'h':
+      usage(argc, argv);
+      exit(EXIT_SUCCESS);
+      break;
+    case 'r':
+      opts->document_root = optarg;
+      break;
+    case 'p':
+      s_http_port = optarg;
+      break;
+    case 'l':
+      opts->enable_directory_listing = "yes";
+      break;
+    
+    default:
+      usage(argc, argv);
+      exit(EXIT_FAILURE);
+      break;
+    }
+  }
+
 }
