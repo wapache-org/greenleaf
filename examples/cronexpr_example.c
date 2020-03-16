b37bac930aaed1f5273fbdeacfd82f419f60abb7
ykuang <ykuang@wapache.org>
2020-03-16 15:09:16 +0800

    fixed trigger time not equal expected time issue

--- examples/cronexpr_example.c
+++ examples/cronexpr_example.c
@@ -7,7 +7,7 @@
 
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
 #define TEST_BASE_TIME "2017-08-29 22:00:20"
-double cs_timegm(const struct tm *tm);
+double mk_time_gmt(const struct tm *tm);
 
 struct test_entry
 {
@@ -70,39 +70,47 @@ static struct test_entry s_te[] = {/* Local time */
     .expected[1] = "2017-10-02 09:00:00",
     .expected[2] = "2017-11-06 09:00:00"}
 };
-struct sorted_test_entry {
-  int id;
-  time_t expected;
-};
+
+/*
+表示时间的3种方式:
+
+time_t : 毫秒数, 没有时区之分
+struct tm : 将时间拆分成了年月日时分秒等部分, 有时区之分, 同样的值用不同的时区去解释会得到不同的time_t
+char*  : 年月日时分秒的字符串格式, , 有时区之分, 同样的值用不同的时区去解释会得到不同的time_t
+
+3者之间的转换
+
+time_t <----> struct tm <----> char*
+
+struct tm 是 time_t 和 char* 的桥梁
+
+ */
+
 
 extern long timezone;
-
-static char s_tz[8] = {0};
-static int s_num_tests = 0;
-static char s_str_time[32] = {0};
-static struct sorted_test_entry *s_sorted_te = NULL;
-static int s_current_id = 0;
-
-static void s_get_timezone(void) {
+static char s_timezone_str[8] = {0};
+static void s_get_timezone() {
   tzset();
   int t = timezone / 60 / 60;
   if (t == 0) {
-    strcpy(s_tz, "UTC");
+    strcpy(s_timezone_str, "UTC");
   } else {
     bool neg = false;
     if (t < 0) {
       neg = true;
       t *= -1;
     }
-    sprintf(s_tz, "UTC%c%d", neg ? '+' : '-', t);
+    sprintf(s_timezone_str, "UTC%c%d", neg ? '+' : '-', t);
   }
 }
 
 static struct tm s_tm = {0};
+/** 字符串 转 tm结构体 */
 static struct tm *s_cron_test_str2tm(const char *date) {
   memset(&s_tm, 0, sizeof s_tm);
 
-  sscanf(date, "%04d-%02d-%02d %02d:%02d:%02d", 
+  sscanf(date, 
+    "%04d-%02d-%02d %02d:%02d:%02d", 
     &s_tm.tm_year, &s_tm.tm_mon, &s_tm.tm_mday, 
     &s_tm.tm_hour, &s_tm.tm_min, &s_tm.tm_sec
   );
@@ -114,36 +122,42 @@ static struct tm *s_cron_test_str2tm(const char *date) {
   return &s_tm;
 }
 
-static time_t s_cron_test_str2timeloc(const char *date) {
+/** 字符串 转 time_t带时区 */
+static time_t s_cron_test_str2time_loc(const char *date) {
   return mktime(s_cron_test_str2tm(date));
 }
 
-static time_t s_cron_test_str2timegm(const char *date) {
-  return (time_t) cs_timegm(s_cron_test_str2tm(date));
+/** 字符串 转 time_t 标准时间 */
+static time_t s_cron_test_str2time_gmt(const char *date) {
+  return (time_t) mk_time_gmt(s_cron_test_str2tm(date));
 }
 
+/** tm结构体 转 字符串(可能带时区或者是标准时间) */
+static char s_str_time[32] = {0};
 static char *s_cron_test_tm2str(struct tm *t, const char *gmt) {
   size_t sz = ARRAY_SIZE(s_str_time);
   memset(s_str_time, 0, sz);
-  snprintf(s_str_time, sz - 1, "%04d-%02d-%02d %02d:%02d:%02d %s",
-           t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min,
-           t->tm_sec, gmt);
+  snprintf(s_str_time, sz - 1, 
+    "%04d-%02d-%02d %02d:%02d:%02d %s",
+    t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, 
+    t->tm_hour, t->tm_min, t->tm_sec, 
+    gmt);
   return s_str_time;
 }
 
-static char *s_cron_test_timeloc2str(time_t date) {
+static char *s_cron_test_loc_time2str(time_t date) {
   struct tm t;
   localtime_r(&date, &t);
-  return s_cron_test_tm2str(&t, s_tz);
+  return s_cron_test_tm2str(&t, s_timezone_str);
 }
 
-static char *s_cron_test_timegm2str(time_t date) {
+static char *s_cron_test_gmt_time2str(time_t date) {
   struct tm t;
   gmtime_r(&date, &t);
   return s_cron_test_tm2str(&t, "UTC");
 }
 
-double cs_timegm(const struct tm *tm) {
+double mk_time_gmt(const struct tm *tm) {
   /* Month-to-day offset for non-leap-years. */
   static const int month_day[12] = {0,   31,  59,  90,  120, 151,
                                     181, 212, 243, 273, 304, 334};
@@ -180,6 +194,9 @@ double cs_timegm(const struct tm *tm) {
 
 int main(int argc, char* argv[])
 {
+  setenv("TZ", "GMT", 1); // 使用标准时间, 不带时区
+  s_get_timezone();
+
     // 
     const char *err = NULL;
 
@@ -194,19 +211,21 @@ int main(int argc, char* argv[])
             exit(1);
         }
 
-        time_t base = s_cron_test_str2timegm(TEST_BASE_TIME);
+        time_t base = s_cron_test_str2time_gmt(TEST_BASE_TIME);
 
         printf("%02d: expr = %s, base_time = %s :\n", index, entry.expr, TEST_BASE_TIME);
+        printf("\tparsed expr: \n");
+        cronexpr_print(expr);
+        printf("\ttrigger times: \n");
 
         for (size_t i = 0; i < 3; i++)
         {
             time_t next = cronexpr_next(expr, base);
-            printf("\tactual = %s, expected = %s UTC\n", s_cron_test_timegm2str(next), entry.expected[i]);
+            char* actual = s_cron_test_gmt_time2str(next);
+            int eq = memcmp(actual, entry.expected[i], strlen(entry.expected[i]));
+            printf("\t\tequal = %s, actual = %s, expected = %s UTC\n", eq? "false":"true", actual, entry.expected[i]);
             base = next;
         }
-        // FIXED 有一部分触发时间不对!!!
-
-        //printf("%s %s %s %s %s %s\n", expr->seconds, expr->minutes, expr->hours, expr->months, expr->days_of_week, expr->days_of_month);
 
         cronexpr_free(expr);
 
