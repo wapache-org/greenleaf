d99462bbe71e648be542d68a91315a3396a16b97
ykuang <ykuang@wapache.org>
2020-03-19 03:24:12 +0800

    add logger functons

---
+++ common/logger.c
@@ -0,0 +1,120 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <time.h>
+
+#include "logger.h"
+
+#ifndef LOG_USE_COLOR
+#define LOG_USE_COLOR 1
+#endif
+
+static struct {
+  void *udata;
+  logger_lock_func lock;
+  FILE *fp;  // 文件描述符
+  int level; // 日志级别 
+  int quiet; // 只输出到文件
+} L;
+
+
+static const char *level_names[] = {
+  "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
+};
+
+#ifdef LOG_USE_COLOR
+static const char *level_colors[] = {
+  "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
+};
+#endif
+
+
+static void lock(void)   {
+  if (L.lock) {
+    L.lock(L.udata, 1);
+  }
+}
+
+
+static void unlock(void) {
+  if (L.lock) {
+    L.lock(L.udata, 0);
+  }
+}
+
+
+void logger_set_udata(void *udata) {
+  L.udata = udata;
+}
+
+
+void logger_set_lock(logger_lock_func fn) {
+  L.lock = fn;
+}
+
+
+void logger_set_fp(FILE *fp) {
+  L.fp = fp;
+}
+
+
+void logger_set_level(int level) {
+  L.level = level;
+}
+
+
+void logger_set_quiet(int enable) {
+  L.quiet = enable ? 1 : 0;
+}
+
+
+void logger_log(int level, const char *file, int line, const char *fmt, ...) {
+  if (level < L.level) {
+    return;
+  }
+
+  /* Acquire lock */
+  lock();
+
+  /* Get current time */
+  time_t t = time(NULL);
+  struct tm *lt = localtime(&t);
+
+  /* Log to stdout/stderr */
+  if (!L.quiet) {
+    FILE* fd = level>3 ? stderr : stdout;
+    va_list args;
+    char buf[16];
+    buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
+#ifdef LOG_USE_COLOR
+    fprintf(
+      fd, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
+      buf, level_colors[level], level_names[level], file, line);
+#else
+    fprintf(fd, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+#endif
+    va_start(args, fmt);
+    vfprintf(fd, fmt, args);
+    va_end(args);
+    fprintf(fd, "\n");
+    fflush(fd);
+  }
+
+  /* Log to file */
+  if (L.fp) {
+    va_list args;
+    char buf[32];
+    buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
+    fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+    va_start(args, fmt);
+    vfprintf(L.fp, fmt, args);
+    va_end(args);
+    fprintf(L.fp, "\n");
+    fflush(L.fp);
+  }
+
+  /* Release lock */
+  unlock();
+}
---
+++ common/logger.h
@@ -0,0 +1,32 @@
+/**
+ * 长期来看, 还是引入log4c比较好
+ */
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#define LOG_VERSION "0.1.0"
+
+typedef void (*logger_lock_func)(void *udata, int lock);
+
+enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+
+#define logger_trace(...) logger_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+#define logger_debug(...) logger_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define logger_info(...)  logger_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
+#define logger_warn(...)  logger_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
+#define logger_error(...) logger_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define logger_fatal(...) logger_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+
+void logger_set_udata(void *udata);
+void logger_set_lock(logger_lock_func fn);
+void logger_set_fp(FILE *fp);
+void logger_set_level(int level);
+void logger_set_quiet(int enable);
+
+void logger_log(int level, const char *file, int line, const char *fmt, ...);
+
+#endif
