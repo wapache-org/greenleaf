adb78802d78a508fd437afe29f3bedab123108dd
ykuang <ykuang@wapache.org>
2020-03-31 23:40:44 +0800

    add sqlite_exec2 test

--- quickjs_modules/sqlite_test.js
+++ quickjs_modules/sqlite_test.js
@@ -1,11 +1,42 @@
 
-var rs;
+//test();
 
-rs = sqlite_exec("CREATE TABLE IF NOT EXISTS kv(key PRIMARY KEY, val);");
-console.log("CREATE", JSON.stringify(rs, undefined, 2));
+test2();
 
-rs = sqlite_exec("INSERT OR REPLACE INTO kv VALUES (1, 'a');");
-console.log("INSERT", JSON.stringify(rs, undefined, 2));
+function test() {
+    var rs;
+    
+    rs = sqlite_exec("CREATE TABLE IF NOT EXISTS kv(key int PRIMARY KEY, val text);");
+    console.log("CREATE", JSON.stringify(rs, undefined, 2));
+    if(rs.code) return;
+    
+    // rs = sqlite_exec("select max(key) as max_key from kv;");
+    // console.log("SELECT", JSON.stringify(rs, undefined, 2));
+    // if(rs.code) return;
+    
+    // max_key = parseInt(rs.rows[0][0]);
+    rs = sqlite_exec2("select max(key) as max_key from kv;");
+    console.log("SELECT", JSON.stringify(rs, undefined, 2));
+    if(rs.code) return;
+    
+    max_key = rs.rows[0][0];
+    rs = sqlite_exec2("INSERT OR REPLACE INTO kv VALUES (?, ?);", max_key+1, 'a');
+    console.log("INSERT", JSON.stringify(rs, undefined, 2));
+    //if(rs.code) return;
+    
+    rs = sqlite_exec2("select * from kv;");
+    console.log("SELECT", JSON.stringify(rs, undefined, 2));
+    if(rs.code) return;
+    
+    rs = sqlite_exec2("delete from kv where key < ?", max_key);
+    console.log("DELETE", JSON.stringify(rs, undefined, 2));
+}
 
-rs = sqlite_exec("select * from kv;");
-console.log("SELECT", JSON.stringify(rs, undefined, 2));
+function test2()
+{
+    var rs;
+
+    rs = sqlite_exec2("select * from metrics_linux_stat;");
+    console.log("SELECT", JSON.stringify(rs, undefined, 2));
+
+}
